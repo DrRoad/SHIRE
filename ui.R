3768ff80c6aa9cc00950aeb2f7cb17f4fc4e843d
Peter Cha <petercha90@gmail.com>
2016-09-04 21:54:56 +0900

    Add more reactive visualization functions.

--- ui.R
+++ ui.R
@@ -2,6 +2,13 @@
 library(shiny)
 
 # Choices for drop-downs
+plots <- c(
+  "Histogram" = "hist",
+  "Boxplot" = "boxplot",
+  "Scatter" = "scatter"
+  )
+
+
 vars <- c(
   "Charges" = "charges",
   "Age" = "age",
@@ -13,33 +20,7 @@ vars <- c(
  
 ui <- fluidPage(navbarPage(title = p(style ="font-family:Impact","Linear Regression"),
     
-    tabPanel(title = "Model producer",
-             
-             sidebarPanel(
-               selectInput(
-                 "plotType", "Plot Type",
-                 c(Scatter = "scatter",
-                   Histogram = "hist")),
-               
-               # Only show this panel if the plot type is a histogram
-               conditionalPanel(
-                 condition = "input.plotType == 'hist'",
-                 selectInput(
-                   "breaks", "Breaks",
-                   c("Sturges",
-                     "Scott",
-                     "Freedman-Diaconis",
-                     "[Custom]" = "custom")),
-                 
-                 # Only show this panel if Custom is selected
-                 conditionalPanel(
-                   condition = "input.breaks == 'custom'",
-                   sliderInput("breakCount", "Break Count", min=1, max=1000, value=10)
-                 )
-               )
-             )         
-             
-    ),
+    tabPanel(title = "Model producer"),
 
     
     # Data exploration.
@@ -50,7 +31,7 @@ ui <- fluidPage(navbarPage(title = p(style ="font-family:Impact","Linear Regress
        column(7, 
              
                 column(5, selectInput(
-                "plotType", "Plot Type",c( Histogram = "hist", Scatter = "scatter"),selected = "hist")
+                "plotType", "Plot Type",plots)
                 ),
                 column(5, selectInput(
                     "variable", "Variables", vars)
