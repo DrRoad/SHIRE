2ba3779f29b53dcc788059156c16e01cb95118ab
Peter Cha <petercha90@gmail.com>
2016-09-14 23:21:29 +0900

    Simple UI modification

--- server.R
+++ server.R
@@ -1,4 +1,5 @@
 
+
 plots <- c(
   "Histogram" = "hist",
   "Boxplot" = "boxplot",
@@ -17,7 +18,7 @@ insurance <- read.csv("insurance.csv")
 
 
 server <- function(input, output, session) {
-
+  
   
   ###From here, Data Explorer
   plot <- reactive(input$plotType)
@@ -27,15 +28,15 @@ server <- function(input, output, session) {
   output$plot <- renderPlot({
     
     if(plot() == "hist"){
-        switch(vari(),
-               charges = hist(insurance$charges, main = paste("Histogram of",vari())),
-               age = hist(insurance$age, main = paste("Histogram of",vari())),
-               bmi = hist(insurance$bmi, main = paste("Histogram of",vari())),
-               children = hist(insurance$children, main = paste("Histogram of",vari()))
-        )
+      switch(vari(),
+             charges = hist(insurance$charges, main = paste("Histogram of",vari())),
+             age = hist(insurance$age, main = paste("Histogram of",vari())),
+             bmi = hist(insurance$bmi, main = paste("Histogram of",vari())),
+             children = hist(insurance$children, main = paste("Histogram of",vari()))
+      )
     }
     else if(plot() == "scatter"){
-        pairs(insurance[c("age","bmi", "children", "charges")], pch= c(1,2,3), main="Scatter plots")
+      pairs(insurance[c("age","bmi", "children", "charges")], pch= c(1,2,3), main="Scatter plots")
       
     }
     else if(plot() == "boxplot"){
@@ -46,83 +47,82 @@ server <- function(input, output, session) {
              children = boxplot(insurance$children, main = paste("Boxplot of",vari()), notch =TRUE)
       )
     }
-
+    
   })
   
-
-     
-   #various informations
-   output$summary <- renderPrint({
-     if(vari() == "age") 
-        summary(insurance$age)
-         
-     else if(vari() == "charges")
-        summary(insurance$charges)
-         
-     else if(vari() == "bmi")
-        summary(insurance$bmi)
-         
-     else if(vari() == "children")
-        summary(insurance$children)
-   })
-   
-   output$table <- renderPrint({
-       table(insurance$region)
-   })
-   output$col <- renderPrint({
-      cor(insurance[c("age", "bmi", "children", "charges")])
-   })
- 
- #data table
- output$dataTable <- renderDataTable(
-   insurance, options = list(pageLength = 5)
- )
- 
- 
- 
- 
- 
- ### From here, Model generator 
-
-   output$result <- renderText({
-       
-       print("File uploaded")
-   })
-   
-   output$model <- renderPlot({
-     if(input$action){hist(insurance$charges)}
-   })
-   
-   
-   output$modelPage <- renderUI({
-     inFile <- input$file1
-     
-     if(is.null(inFile))
-       return(NULL)
-     
-     read.csv(inFile$datapath, header = input$header)
+  
+  
+  #various informations
+  output$summary <- renderPrint({
+    if(vari() == "age") 
+      summary(insurance$age)
     
-     #  A defendent variable
-     wellPanel(
-       radioButtons("dfvari", 
-                    label = "DV(Defendent variable)", 
-                    choices = vars, 
-                    selected = "charges", 
-                    inline=TRUE, 
-                    width = '300px'),
-       
-       
-       
-       #  Explanatory variables. 
-       radioButtons("exvaris", "EV(Explanatory variables):",
-                    c("all-except DV","except factors","select"), inline = TRUE),
-       
-       conditionalPanel(
-         condition = "input.exvaris == 'select'",
-         checkboxGroupInput("selectedExvaris","Select EVs", vars, inline= TRUE)
-       ),
-       actionButton("action","Generate Model ",icon("refresh"))
-     )
-   })
-
- }
+    else if(vari() == "charges")
+      summary(insurance$charges)
+    
+    else if(vari() == "bmi")
+      summary(insurance$bmi)
+    
+    else if(vari() == "children")
+      summary(insurance$children)
+  })
+  
+  output$table <- renderPrint({
+    table(insurance$region)
+  })
+  output$col <- renderPrint({
+    cor(insurance[c("age", "bmi", "children", "charges")])
+  })
+  
+  #data table
+  output$dataTable <- renderDataTable(
+    insurance, options = list(pageLength = 5)
+  )
+  
+  
+  
+  
+  
+  ### From here, Model generator 
+  
+  output$modelPage <- renderUI({
+    inFile <- input$file1
+    
+    if(is.null(inFile))
+      return(NULL)
+    
+    output$result <- renderText({
+      print("File uploaded")
+    })
+    
+    output$model <- renderPlot({
+      if(input$action){hist(insurance$charges)}
+    })
+    
+    
+    read.csv(inFile$datapath, header = input$header)
+    
+    #  A defendent variable
+    wellPanel(
+      radioButtons("dfvari", 
+                   label = "DV(Defendent variable)", 
+                   choices = vars, 
+                   selected = "charges", 
+                   inline=TRUE, 
+                   width = '300px'),
+      
+      
+      
+      #  Explanatory variables. 
+      radioButtons("exvaris", "EV(Explanatory variables):",
+                   c("all-except DV","except factors","select"), inline = TRUE),
+      
+      conditionalPanel(
+        condition = "input.exvaris == 'select'",
+        checkboxGroupInput("selectedExvaris","Select EVs", vars, inline= TRUE)
+      ),
+      actionButton("action","Generate Model ",icon("refresh"))
+    )
+  })
+  
+}
--- ui.R
+++ ui.R
@@ -6,7 +6,7 @@ plots <- c(
   "Histogram" = "hist",
   "Boxplot" = "boxplot",
   "Scatter" = "scatter"
-  )
+)
 
 
 vars <- c(
@@ -17,64 +17,56 @@ vars <- c(
 )
 
 
- 
-ui <- fluidPage(navbarPage(title = p(style =" font-family:Impact","Linear Regression"),
-    
-    # model generator 
-                           
-    tabPanel(title = "Model generator",
-      
-       fluidRow(
-          column(6, uiOutput("modelPage")),
-          column(6,
-                 conditionalPanel(
-                   condition = "input.action == TRUE",
-                   plotOutput("model"))
-          )
-       ),
-       fluidRow(
-          column(3,
-            fileInput("file1", "Choose CSV File",multiple = FALSE, 
-                      accept = c("text/csv","text/comma-separated-values,text/plain",".csv"),
-                      width = '200px')
-            ),
-          column(3,checkboxInput("header", "Header", TRUE),
-                 textOutput("result"))
-         )
-    ),
-    
-    
-    # Data exploration.
-    
-    tabPanel("Data explorer",
-   
-     fluidRow(
-       column(7, 
-             
-                column(5, selectInput(
-                "plotType", "Plot Type",plots)
-                ),
-                column(5, selectInput(
-                    "variable", "Variables", vars)
-                ),
-                plotOutput("plot")
-          ),
-          
-        column(5, p(style ="font-family:Impact","Summary"),
-                  verbatimTextOutput("summary"),br(),
-                  p(style ="font-family:Impact","Regional Data & Correlations"),
-                  verbatimTextOutput("table"),
-                  verbatimTextOutput("col")
-          )
-        
-      ),br(),br(),br(),hr(),
-     
-     fluidRow(
-         column(12, align = "center",dataTableOutput('dataTable')),
-         hr()
-       )
-     
-     
-    )
 
+ui <- fluidPage(navbarPage(title = p(style =" font-family:Impact","Linear Regression"),
+                           
+   # model generator 
+                           
+  tabPanel(title = "Model generator",
+                                    
+        
+           
+           fluidRow(
+             column(3,
+                    fileInput("file1", "Choose CSV File",multiple = FALSE, 
+                              accept = c("text/csv","text/comma-separated-values,text/plain",".csv"),
+                              width = '200px')
+             ),
+             column(3,
+                    checkboxInput("header", "Header", TRUE),
+                    textOutput("result"))
+           ),
+           
+           fluidRow(
+                 column(6, uiOutput("modelPage")),
+                 column(6,
+                        conditionalPanel(
+                            condition = "input.action == TRUE",
+                            plotOutput("model"))
+                        )
+           )
+        ),
+   
+        # Data exploration.
+                           
+         tabPanel("Data explorer",
+                  fluidRow(
+                          column(7, 
+                                  column(5, selectInput("plotType", "Plot Type",plots)),
+                                  column(5, 
+                                         selectInput("variable", "Variables", vars)),
+                                  plotOutput("plot")
+                          ),
+                          column(5, p(style ="font-family:Impact","Summary"),
+                                             verbatimTextOutput("summary"),br(),
+                                             p(style ="font-family:Impact","Regional Data & Correlations"),
+                                             verbatimTextOutput("table"),
+                                             verbatimTextOutput("col")
+                  )),br(),br(),br(),hr(),
+                                    
+                  fluidRow(
+                          column(12, align = "center",dataTableOutput('dataTable')),
+                          hr()
+                  )
+         )
 ))
