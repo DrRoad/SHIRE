160bfb2c15da0428d718c3615d6004a30c67e4bd
Peter Cha <petercha90@gmail.com>
2016-12-05 23:57:25 +0900

    Add files via upload

--- intro/server.R
+++ intro/server.R
@@ -1,8 +1,33 @@
  library(shiny)
  library(shinyjs)
- 
- server = function(input, output) {
+ library(shinyBS)
+server = function(input, output,session) {
+
+	shinyjs::addClass(id = 'menus', class = "navbar-right")
+
+	# bsModal("greeting", "Greeting", input$hello, size = "large",tags$h1("hello"))
+    
+	
     observe({
       toggle(condition = input$foo, selector = "#navbar li a[data-value=tab2]")
     })
-  }
+
+
+    # output$greeting <- render
+
+    # observe({
+    # 	if(input&menus == "A"){
+    # 	showModal(modalDialog(
+    # 		title  = "Hi, there!",
+    # 		"This is a greeting.",
+    # 		easyClose = TRUE))
+    # 	}
+    # })
+
+
+
+
+
+
+
+}
--- intro/ui.R
+++ intro/ui.R
@@ -1,46 +1,102 @@
 library(shiny)
 library(shinyjs)
+library(shinyBS)
 library(shinythemes)
 
 
 ui <- fluidPage( style = "background-color:white",
   tags$head(
     tags$style(HTML("
-     
-     body{
-      background-attachment: fixed;
-      background-image: url('intro.jpg');
-      background-size: auto 100%;
-      background-position: center top;
-      background-color: rgb(245, 248, 250);
-     }
-     .navbar { 
-        margin-bottom : 0px;
-        margin-left : -15px;
-        margin-right: -15px;
+      strong{
+          text-align: center;
+          text-color: black;
       }
-     .shiny-output-error { visibility: hidden; } 
-     .shiny-output-error:before { visibility: hidden; }
-     .navbar.navbar.navbar-fixed-top.app-navbar {
-          background-color: #ffffff;
-          border-bottom-color: #bbb;
-     }
-     #icon{
-      margin-top : -13px;
-     }
-  "))
-),theme = shinytheme("flatly"),navbarPage(title = img(id="icon", src="icon.PNG", height= 48, width= 100),
-  
-      # Data exploration.
-                         
-       tabPanel("Data explorer"
-                           
-        ),
-        # model generator.  
+      img {
+        display : block;
+        margin: auto;
+      }
+      img#logo {
+        margin-top : 12%;
+        margin-bottom: 30px;
 
-        tabPanel(title = "Model generator"
-            
-        
+      }
+      .navbar { 
+          margin-bottom : 0px;
+          margin-left : -15px;
+          margin-right: -15px;
+
+      }
+      .shiny-output-error { visibility: hidden; } 
+      .shiny-output-error:before { visibility: hidden; }
+      .navbar.navbar.navbar-fixed-top.app-navbar {
+            background-color: #ffffff;
+            border-bottom-color: #bbb;
+      }
+      .text-center {
+          text-align: center;
+      }
+      #icon{
+        margin-top : -30px;
+      }
+      div#init-text {
+          opacity: 1;
+          font-weight: normal;
+          position: center;
+      }
+      
+
+  "))
+    #menus > li:first-child { display: none; }
+
+),theme = shinytheme("simplex"),useShinyjs(),navbarPage(title = tags$a(href="/",img(id="icon", src="icon.PNG", height= 80, width= 110)),collapsible = TRUE, id = "menus",
+  
+      # Home                
+      tabPanel("Welcome", 
+        style = 
+         "background-attachment: fixed;
+          background-image: url('intro.jpg');
+          background-size: auto 100%;
+          background-position: center top;
+          background-color: rgb(245, 248, 250);
+          padding-right: 0px;
+          margin-right : -30px;
+          margin-left : -30px;
+          align-items: center;",
+
+        fluidRow( 
+          column(12, align="center",
+            tags$img(id ="logo",
+              height = "100px",
+              width = "auto",
+              src = "logo.png"
+            ),
+            strong(p(style="color:#ffffff","Shire is a machine learning tool",br(),
+            "for any data scientist or student who",br(),"want to utilize various Machine Learning techniques",br(),"by using Shiny of Rstudio."))
+
+            ,selectInput("techniques", "Select technique", list(
+              "Supervised" = c("Linear regression", "Logistic regression", "Naive Bayes classifier","Artificial neural network", "Decision trees","k-nearest neighbor", "Support vector machines", "Random Forests","Boosting
+                "),
+              "Unsupervised" = c("Association rule learning", "K-means algorithm","Principal Components Analysis")
+              )
+            ),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br()
           )
+        ),
+
+        fluidRow(
+            column(12, 
+              tags$img(
+                height = "60px",
+                width = "auto",
+                src = "copyright.png"
+               ) 
+            )   
+         )
+      ),
+
+      # Greeting 
+      tabPanel("About"
       )
+            
+
+  )
 )
