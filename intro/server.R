ed49e396cddfbec65cd2d5bd37a9a42cdcbc3a00
Peter Cha <petercha90@gmail.com>
2016-12-06 23:00:27 +0900

    Add files via upload

--- intro/server.R
+++ intro/server.R
@@ -5,6 +5,7 @@ server = function(input, output,session) {
 
 	shinyjs::addClass(id = 'menus', class = "navbar-right")
 
+
 	# bsModal("greeting", "Greeting", input$hello, size = "large",tags$h1("hello"))
     
 	
@@ -12,16 +13,11 @@ server = function(input, output,session) {
       toggle(condition = input$foo, selector = "#navbar li a[data-value=tab2]")
     })
 
+   
+    # observeEvent(input$techniques,{
 
-    # output$greeting <- render
 
-    # observe({
-    # 	if(input&menus == "A"){
-    # 	showModal(modalDialog(
-    # 		title  = "Hi, there!",
-    # 		"This is a greeting.",
-    # 		easyClose = TRUE))
-    # 	}
+
     # })
 
 
--- intro/ui.R
+++ intro/ui.R
@@ -5,8 +5,18 @@ library(shinythemes)
 
 
 ui <- fluidPage( style = "background-color:white",
-  tags$head(
+  tags$head(tags$title("SHIRE"),
     tags$style(HTML("
+      
+      body {
+          padding-right : 0px;
+          padding-left : 0px;
+          background-attachment: fixed;
+          background-image: url('intro.jpg');
+          background-size: auto 100%;
+          background-position: center top;
+          background-color: rgb(245, 248, 250);
+      }
       strong{
           text-align: center;
           text-color: black;
@@ -15,16 +25,10 @@ ui <- fluidPage( style = "background-color:white",
         display : block;
         margin: auto;
       }
-      img#logo {
-        margin-top : 12%;
-        margin-bottom: 30px;
-
-      }
       .navbar { 
           margin-bottom : 0px;
           margin-left : -15px;
           margin-right: -15px;
-
       }
       .shiny-output-error { visibility: hidden; } 
       .shiny-output-error:before { visibility: hidden; }
@@ -38,54 +42,67 @@ ui <- fluidPage( style = "background-color:white",
       #icon{
         margin-top : -30px;
       }
+      #techniques {
+          align : left;
+      }
       div#init-text {
           opacity: 1;
           font-weight: normal;
           position: center;
       }
-      
-
+      #title {
+        position : fixed; 
+        top : 28%;
+        left: 15px;
+        right : 15px;
+        color:#000000;
+        z-index : 1;
+      }
+      #question {
+        font-size : 15px;
+        text-align: left;
+      }
+      #footer
+      {
+          position:fixed;
+          left : 15px;
+          right : 15px;
+          bottom:10px;
+      }
   "))
     #menus > li:first-child { display: none; }
 
-),theme = shinytheme("simplex"),useShinyjs(),navbarPage(title = tags$a(href="/",img(id="icon", src="icon.PNG", height= 80, width= 110)),collapsible = TRUE, id = "menus",
+),theme = shinytheme("simplex"),useShinyjs(),navbarPage(title = tags$a(href="/",img(id="icon", src="icon.PNG", height= 80, width= 110)),collapsible = TRUE, id = "menus", 
   
       # Home                
-      tabPanel("Welcome", 
-        style = 
-         "background-attachment: fixed;
-          background-image: url('intro.jpg');
-          background-size: auto 100%;
-          background-position: center top;
-          background-color: rgb(245, 248, 250);
-          padding-right: 0px;
-          margin-right : -30px;
-          margin-left : -30px;
-          align-items: center;",
+      tabPanel("Welcome", style="margin-bottom: -5px;",
+        fluidRow(
+          column(12,align='center',
+            
+            tags$div(id="title", 
+              tags$img(id ="logo",
+                  height = "100px",
+                  width = "auto",
+                  src = "logo.png"
+              ),br(),br(),
+              strong(p(style="color:#ffffff","Shire is a machine learning tool",br(),
+              "for any data scientist or student who",br(),"want to utilize various Machine Learning techniques",br(),"by using Shiny of Rstudio."))
 
-        fluidRow( 
-          column(12, align="center",
-            tags$img(id ="logo",
-              height = "100px",
-              width = "auto",
-              src = "logo.png"
-            ),
-            strong(p(style="color:#ffffff","Shire is a machine learning tool",br(),
-            "for any data scientist or student who",br(),"want to utilize various Machine Learning techniques",br(),"by using Shiny of Rstudio."))
-
-            ,selectInput("techniques", "Select technique", list(
-              "Supervised" = c("Linear regression", "Logistic regression", "Naive Bayes classifier","Artificial neural network", "Decision trees","k-nearest neighbor", "Support vector machines", "Random Forests","Boosting
-                "),
-              "Unsupervised" = c("Association rule learning", "K-means algorithm","Principal Components Analysis")
-              )
-            ),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br()
+              ,selectInput("techniques", tags$div(id="question", style ="align: right;",strong("Select a technique _____________________",icon("question-sign", lib = "glyphicon"))), list(
+                "SUPERVISED" = c("Linear regression", "Logistic regression", "Naive Bayes classifier","Artificial neural network", "Decision trees","k-nearest neighbor", "Support vector machines", "Random Forests","Boosting
+                  "),
+                "UNSUPERVISED" = c("Association rule learning", "K-means algorithm","Principal Components Analysis")
+                )),
+                bsTooltip("question", 'If you need a target value for any new input, "SUPERVISED" techniques will be needed. Contrary, if you need to find the structure or relationships between different inputs, you need to use "UNSUPERVISED" techniques.',
+                  "right", options = list(container = "body"))
+            )
           )
         ),
 
         fluidRow(
             column(12, 
-              tags$img(
-                height = "60px",
+              tags$img(id='footer',
+                height = "35px",
                 width = "auto",
                 src = "copyright.png"
                ) 
