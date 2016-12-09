b23ce324e78ee0c29c0a43e2742362977ed7796b
Peter Cha <petercha90@gmail.com>
2016-12-09 23:21:49 +0900

    Add files via upload

--- app.R
+++ app.R
@@ -20,8 +20,9 @@ ui <- fluidPage( style = "background-color:white",
     tags$style(HTML("
       
 	      body {
-	          padding-right : 0px;
-	          padding-left : 0px;
+	          padding-right : 15px;
+	          padding-left : 15px;
+	          padding-bottom : 60px;
 	          background-attachment: fixed;
 	          background-image: url('intro.jpg');
 	          background-size: auto 100%;
@@ -36,13 +37,13 @@ ui <- fluidPage( style = "background-color:white",
 	        display : block;
 	        margin: auto;
 	      }
+	      .shiny-output-error { visibility: hidden; } 
+     	  .shiny-output-error:before { visibility: hidden; }
 	      .navbar { 
 	          margin-bottom : 0px;
-	          margin-left : -15px;
-	          margin-right: -15px;
+	          margin-left : -30px;
+	          margin-right: -25px;
 	      }
-	      .shiny-output-error { visibility: hidden; } 
-	      .shiny-output-error:before { visibility: hidden; }
 	      .navbar.navbar.navbar-fixed-top.app-navbar {
 	            background-color: #ffffff;
 	            border-bottom-color: #bbb;
@@ -84,14 +85,13 @@ ui <- fluidPage( style = "background-color:white",
 	          left : 15px;
 	          right : 15px;
 	          bottom:10px;
+	          z-index : -1;
 	      }
 	      #controls {
 	        /* Appearance */
 	        background-color: Lavender;
 	        padding: 0px 0px 0px 0px;
-	        margin-top : -1%;
-	        margin-right: -7%;
-	        
+	        margin-left : -20px;
 	        cursor: move;
 	        position: absolute;
 	        z-index:3;
@@ -108,13 +108,22 @@ ui <- fluidPage( style = "background-color:white",
 	  "))
     #menus > li:first-child { display: none; }
 
-	),theme = shinytheme("simplex"),useShinyjs(),
+	),theme =  shinytheme("simplex"),useShinyjs(),
   	navbarPage(title = tags$a(href="/",img(id="icon", src="icon.PNG", height= 80, width= 110)),collapsible = TRUE, id = "menus", 
 	  
 	    # include the UI for each tab
 	    source(file.path("ui", "main.R"),  local = TRUE)$value,
 	    source(file.path("ui", "data_explorer.R"),  local = TRUE)$value,
-	    source(file.path("ui", "linear_regression.R"),  local = TRUE)$value
+	    source(file.path("ui", "linear_regression.R"),  local = TRUE)$value,
+	    fluidRow(
+	      column(12, 
+	        tags$img(id='footer',
+	          height = "35px",
+	          width = "auto",
+	          src = "copyright.png"
+	         ) 
+	      )   
+	    )
 
 	    
 	)
