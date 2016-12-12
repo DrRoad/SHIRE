57bd9bf700159bde82f43f6e3f1c8f046102addb
Peter Cha <petercha90@gmail.com>
2016-12-12 20:40:48 +0900

    Add files via upload

--- app.R
+++ app.R
@@ -15,13 +15,16 @@ plots <- c(
   "Scatter" = "scatter"
 )
 
-ui <- fluidPage( style = "background-color:white",
+ui <- fluidPage( style = "background-color:white",useShinyjs(),
   tags$head(tags$title("SHIRE"),
     tags$style(HTML("
       
 	      body {
-	          padding-right : 0px;
-	          padding-left : 0px;
+	      	  padding-top : 10px;
+	          padding-right : 15px;
+	          padding-left : 15px;
+	          padding-bottom : 60px;
+	          margin-top : -15px;
 	          background-attachment: fixed;
 	          background-image: url('intro.jpg');
 	          background-size: auto 100%;
@@ -36,13 +39,14 @@ ui <- fluidPage( style = "background-color:white",
 	        display : block;
 	        margin: auto;
 	      }
-	      .navbar { 
-	          margin-bottom : 0px;
-	          margin-left : -15px;
-	          margin-right: -15px;
-	      }
 	      .shiny-output-error { visibility: hidden; } 
-	      .shiny-output-error:before { visibility: hidden; }
+     	  .shiny-output-error:before { visibility: hidden; }
+	      .navbar { 
+	      	  padding-top : 5px;
+	          margin-bottom : -5px;
+	          margin-left : -30px;
+	          margin-right: -30px;
+	      }
 	      .navbar.navbar.navbar-fixed-top.app-navbar {
 	            background-color: #ffffff;
 	            border-bottom-color: #bbb;
@@ -84,14 +88,13 @@ ui <- fluidPage( style = "background-color:white",
 	          left : 15px;
 	          right : 15px;
 	          bottom:10px;
+	          z-index : -1;
 	      }
 	      #controls {
 	        /* Appearance */
 	        background-color: Lavender;
 	        padding: 0px 0px 0px 0px;
-	        margin-top : -1%;
-	        margin-right: -7%;
-	        
+	        margin-left : -20px;
 	        cursor: move;
 	        position: absolute;
 	        z-index:3;
@@ -105,16 +108,27 @@ ui <- fluidPage( style = "background-color:white",
 	        opacity: 0.95;
 	        transition-delay: 0;
 	      }
+	      
 	  "))
     #menus > li:first-child { display: none; }
 
-	),theme = shinytheme("simplex"),useShinyjs(),
+	),theme =  shinytheme("simplex"),
   	navbarPage(title = tags$a(href="/",img(id="icon", src="icon.PNG", height= 80, width= 110)),collapsible = TRUE, id = "menus", 
 	  
 	    # include the UI for each tab
 	    source(file.path("ui", "main.R"),  local = TRUE)$value,
 	    source(file.path("ui", "data_explorer.R"),  local = TRUE)$value,
-	    source(file.path("ui", "linear_regression.R"),  local = TRUE)$value
+	    source(file.path("ui", "linear_regression.R"),  local = TRUE)$value,
+	    source(file.path("ui", "logistic_regression.R"),  local = TRUE)$value,
+	    fluidRow(
+	      column(12, 
+	        tags$img(id='footer',
+	          height = "32px",
+	          width = "auto",
+	          src = "copyright.png"
+	         ) 
+	      )   
+	    )
 
 	    
 	)
@@ -138,6 +152,7 @@ server <- function(input, output, session) {
   p.value <- c()
   except_factors <- c()
   factors <- c()
+  childNum <- ""
 
   reacVars <- eventReactive(input$insertVariBox,{})
   evMethod <<- reactive(input$exvaris)
@@ -147,6 +162,9 @@ server <- function(input, output, session) {
   plotSelected <<- reactive(input$plotType)
   variSelected <<- reactive(input$variable)
   factorSelected <<- reactive(input$factorSelector)
+  techSelected <<- reactive(input$techniques1)
+  techSelected2 <<- reactive(input$techniques2)
+  
   #New Column Section ###
   #튜닝변수 설정.
   newName <<- reactive(input$newMemName)
